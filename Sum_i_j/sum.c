67d6069b463f5991946af4130777269e38a93f38
SutharMahendra <omajimahendra@gmail.com>
2023-11-23 21:48:56 +0530

    Sum of I and J

---
+++ Sum_i_j/sum.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main(){
+    int arr[5][5]={
+    {1,2,3,4,5},
+    {2,4,3,4,5},
+    {2,3,4,5,3},
+    {3,5,6,7,8},
+    {5,6,7,8,9}
+    };
+    int sum=0;
+    printf("Sum of row");
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            sum+=arr[i][j];
+        }
+        printf("sum row:%d",sum);
+        sum=0;
+    }
+
+    printf("sum of column");
+    int i;
+
+    for (int j = 0; j < 5; j++){
+        for (i = 0; i < 5; i++);
+        {
+            sum+=arr[i][j];
+        }
+        printf("sum column: %d", sum);
+        sum=0;
+        
+    }
+    
+   return 0; 
+}
