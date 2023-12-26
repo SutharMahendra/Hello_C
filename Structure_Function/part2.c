9d687ecc332689b20dcb91b99dda7b9a0a5415cf
SutharMahendra <omajimahendra@gmail.com>
2023-12-26 13:27:35 +0530

    pass structure variable as argument

---
+++ Structure_Function/part2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+//pass structure variable as argument
+struct part2
+{
+    /* data */
+    int x;
+    int y;
+    int z;
+};
+
+void display(struct part2 value){
+   printf("%d %d %d", value.x, value.y , value.z);
+}
+
+int main(){
+    struct part2 p1 = {12,13,14};
+    struct part2 p2 = {12,34,55};
+    
+    display(p1);
+    display(p2);
+    return 0;  
+    
+}
